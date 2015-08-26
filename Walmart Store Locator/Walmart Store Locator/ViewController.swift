3f417d18fde26dbe57b3ce60068af86ba8545d17
heoiu87 <brian_2447@hotmail.com>
2015-08-25 22:41:22 -0500

    Added http request using ZIP code

--- Walmart Store Locator/Walmart Store Locator/ViewController.swift
+++ Walmart Store Locator/Walmart Store Locator/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NSURLConnectionDelegate {
 
     @IBOutlet weak var CityNameTextBox: UITextField!
     @IBOutlet weak var ZIPcodeTextBox: UITextField!
     @IBOutlet weak var LocationsTextView: UITextView!
+    lazy var data = NSMutableData()
+    var locations = Array<NSDictionary>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,8 +27,29 @@ class ViewController: UIViewController {
     }
 
     @IBAction func SearchButton(sender: UIButton) {
-        
+        var input = ZIPcodeTextBox.text
+        startConnection(input)
     }
-
+    
+    func startConnection(zipcode:String) {
+        let urlPath: String = "http://api.walmartlabs.com/v1/stores?apiKey=teeauzr2tm7867rfk6atybug&zip=\(zipcode)&format=json"
+        var url: NSURL = NSURL(string: urlPath)!
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)!
+        connection.start()
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!){
+        self.data.appendData(data)
+    }
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        var err: NSError
+        // throwing an error on the line below (can't figure out where the error message is)
+        var jsonResult: Array<NSDictionary> = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as! Array<NSDictionary>
+        locations = jsonResult
+        println(locations)
+    }
+    
+    
 }
 
